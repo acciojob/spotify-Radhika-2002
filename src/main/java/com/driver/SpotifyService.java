7264f86de8803ac870ef74a23e7dd234602c59fd
Radhika-2002 <2019radhika.katiyara@ves.ac.in>
2024-03-03 13:41:57 +0530

    Update SpotifyService.java

--- src/main/java/com/driver/SpotifyService.java
+++ src/main/java/com/driver/SpotifyService.java
@@ -1,50 +1,53 @@
 package com.driver;
 
-import java.util.List;
+import java.util.*;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class SpotifyService {
 
+    //Auto-wire will not work in this case, no need to change this and add autowire
+
     SpotifyRepository spotifyRepository = new SpotifyRepository();
 
-    public User createUser(String name, String mobile) {
-        return null;
+    public User createUser(String name, String mobile){
+
     }
 
     public Artist createArtist(String name) {
-        return null;
+
     }
 
     public Album createAlbum(String title, String artistName) {
-        return null;
+
     }
 
     public Song createSong(String title, String albumName, int length) throws Exception {
-        return null;
+
     }
 
     public Playlist createPlaylistOnLength(String mobile, String title, int length) throws Exception {
-        return null;
+
     }
 
     public Playlist createPlaylistOnName(String mobile, String title, List<String> songTitles) throws Exception {
-        return null;
+
     }
 
     public Playlist findPlaylist(String mobile, String playlistTitle) throws Exception {
-        return null;
+
     }
 
     public Song likeSong(String mobile, String songTitle) throws Exception {
-        return null;
+
     }
 
     public String mostPopularArtist() {
-        return null;
+
     }
 
     public String mostPopularSong() {
-        return null;
+
     }
 }
